01d11be0a9615b3f466e41829f3df5502df886b8
dianacamacho <dcam1901@yahoo.com>
2017-01-02 23:55:49 -0600

    unity game, number wizard. use up/down keys to te to adjust guess values

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	public int maxGuesses = 5;
+	public Text text;
+	 
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+
+
+	public void GuessLower ()
+	{
+		max = guess;
+		NextGuess();
+	}
+
+	public void GuessHigher ()
+	{
+		min = guess;
+		NextGuess();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min, max);
+		text.text = guess.ToString();
+	}
+
+	void NextGuess () {
+		guess = Random.Range(min, max + 1);
+		text.text = guess.ToString();
+		maxGuesses = maxGuesses - 1;
+
+		if (maxGuesses <= 0) {
+			SceneManager.LoadScene("Win");
+//			Application.LoadLevel("Win");
+		}
+	}
+}
